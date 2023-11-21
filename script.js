3bc3c77ae5d678fcc980a0f72cab9f7ad70e265b
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-21 22:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Prompt for confirmation
+var userConfirmation = confirm("Do you want to proceed?");
+
+// Check user's choic
+if (userConfirmation) {
+    // Display alert if OK is clicked
+    alert("You clicked OK. Proceeding...");
+} else {
+    // Display alert if Cancel is clicked
+    alert("You clicked Cancel. Exiting...");
+}
+
